5f9b4240e87a137d8d63367de4c61310cd8a6727
Sol Battaglia <solbattaglia@gmail.com>
2023-06-22 08:21:00 -0300

    start with recommendations

--- dags/get_recomendations.py
+++ dags/get_recomendations.py
@@ -1,12 +1,9 @@
 # 'https://api.spotify.com/v1/recommendations?seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA'
-# BQAap8Vs4jYZU_T9ou0fLjRvkPCpThQLXzXVC3CbS-p-07BvieU3DLhYIbtvVMqdmo6QFSvx-M13Wy1bx3Fa7jbEFDNOKMQaKfspzYR1t3ksR28-YqQ
 # curl --request GET \
 #   --url 'https://api.spotify.com/v1/recommendations?seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA' \
-#   --header 'Authorization: Bearer BQAap8Vs4jYZU_T9ou0fLjRvkPCpThQLXzXVC3CbS-p-07BvieU3DLhYIbtvVMqdmo6QFSvx-M13Wy1bx3Fa7jbEFDNOKMQaKfspzYR1t3ksR28-YqQ'
 
 # curl --request GET \
 #   --url https://api.spotify.com/v1/me/top/artists \
-#   --header 'Authorization: Bearer BQAD9cUflfgsa-FUFg5hTvDxACuJrVOsQjtVul9NMjVWJOIqp5UQKLa_f_iG94vNr2FgchlNwsVoBcA1DvA1XuItEFLSCBLu0u2RAdGifzVHh7msHWE'
 
 # BQAD9cUflfgsa-FUFg5hTvDxACuJrVOsQjtVul9NMjVWJOIqp5UQKLa_f_iG94vNr2FgchlNwsVoBcA1DvA1XuItEFLSCBLu0u2RAdGifzVHh7msHWE
 
